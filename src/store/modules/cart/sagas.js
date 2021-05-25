0b15bc75526a5d7d256ca40e436d49cb1d38dfc3
Cassio <cassioate@gmail.com>
2021-05-25 15:03:14 -0300

    Refatorando codigo, trazendo a logica do addToCart para o SAGA no lugar do REDUCER, visando deixar o reducer cada vez mais simples

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -1,15 +1,32 @@
-import { all, call, put, takeLatest } from 'redux-saga/effects'
+import { all, select, call, put, takeLatest } from 'redux-saga/effects'
 import api from '../../../services/api'
+import { formatPrice } from '../../../util/format'
 
-import {addToCartSucess} from './actionsCart'
+import {addToCartSucess, updateAmount} from './actionsCart'
 
 // esse function* é igual a utilizar async function, só que o function* é mais potente
 // o ID está com {id} pois vc está buscando apenas o id da ActionCart, lá virá um type e um id como objeto, mas vc só quer o id. (Destructuring)
 function* addToCart( { id } ) {
-    // yield seria igual ao await
-    const response = yield call(api.get, `/products/${id}`)
+    const productExists = yield select (
+        // ReducerCart pois é como está na rootReducere
+        state => state.ReducerCart.find(p => p.id === id)
+    )
+    
+    if (productExists) {
+        const amount = productExists.amount + 1;
+        yield put(updateAmount(id, amount))
+    } else {
+        // yield seria igual ao await
+        const response = yield call(api.get, `/products/${id}`)
 
-    yield put(addToCartSucess(response.data));
+        const data = {
+            ...response.data,
+            amount: 1,
+            priceFormatted: formatPrice(response.data.price)
+        }
+
+        yield put(addToCartSucess(data));
+    }
 }
 
 export default all([
